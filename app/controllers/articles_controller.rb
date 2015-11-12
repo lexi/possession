505b951b2e4731bd9f5de9b7f7d5cf220268e1e3
Alexis Fellenius <alexis.fellenius@oktavilla.se>
2015-11-12 19:44:06 +0100

    Go Seattle style

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -4,7 +4,7 @@ class ArticlesController < ApplicationController
   end
 
   def show
-    @article = Article.find(params[:id])
+    @article = Article.find params[:id]
   end
 
   def new
@@ -12,11 +12,11 @@ class ArticlesController < ApplicationController
   end
 
   def edit
-    @article = Article.find(params[:id])
+    @article = Article.find params[:id]
   end
 
   def create
-    @article = Article.new(article_params)
+    @article = Article.new article_params
 
     if @article.save
       redirect_to @article
@@ -25,10 +25,10 @@ class ArticlesController < ApplicationController
     end
   end
 
-  def update
-    @article = Article.find(params[:id])
+  def self.update
+    @article = Article.find params[:id]
 
-    if @article.update(article_params)
+    if @article.update article_params
       redirect_to @article
     else
       render 'edit'
@@ -36,7 +36,7 @@ class ArticlesController < ApplicationController
   end
 
   def destroy
-    @article = Article.find(params[:id])
+    @article = Article.find params[:id]
     @article.destroy
 
     redirect_to articles_path
@@ -44,6 +44,6 @@ class ArticlesController < ApplicationController
 
   private
     def article_params
-      params.require(:article).permit(:title, :text)
+      params.require(:article).permit :title, :text
     end
 end
